961a5125c7e832fdbea053f080243f09d76ed732
pycckuu <is.markelov@gmail.com>
2014-03-22 00:32:47 +0100

    Forward Euler working

--- ForwardEulerSolver.cpp
+++ ForwardEulerSolver.cpp
@@ -1,14 +1,17 @@
 #include "ForwardEulerSolver.hpp"
 #include "iostream"
 
-double RightHandSide(double y, double t){
+double ForwardEulerSolver::RightHandSide(double y, double t){
+  // std::cout<< "time: " << 1 + t << "\n";
   return 1 + t;
 }
 double ForwardEulerSolver::SolveEquation() {
   current_value = initialValue;
   for (current_time = initialTime; current_time < finalTime; current_time += stepSize)
   {
-    current_value = initialValue + stepSize * RightHandSide(current_value, current_time);
+    current_value = initialValue + stepSize * ForwardEulerSolver::RightHandSide(current_value, current_time);
+    std::cout << "current value: " << current_value << "\n";
+    initialValue = current_value;
   }  
   return 0;
 }
--- main.cpp
+++ main.cpp
@@ -1,12 +1,15 @@
 #include "stdio.h"
 #include "OdeSolver.hpp"
 #include "ForwardEulerSolver.hpp"
-#include "iostream"
+#include <iostream>
 
 int main(int argc, char const *argv[])
 {
   ForwardEulerSolver example;
-  example.SetStepSize(0.1);
-  std::cout << example.stepSize;
+  example.stepSize = 0.00001;
+  example.initialTime = 0;
+  example.finalTime = 1;
+  example.initialValue = -1;
+  example.SolveEquation();
   return 0;
 }
